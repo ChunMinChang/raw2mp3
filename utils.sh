11730605749cf2da79e5209f1e70c2dca9c108d9
Chun-Min Chang <chun.m.chang@gmail.com>
2019-01-28 13:34:47 -0800

    Show color on Windows Bash

--- utils.sh
+++ utils.sh
@@ -22,28 +22,28 @@ function print_error() {
   local msg=$1
   local bold_red="\033[1;31m"
   local normal="\033[0m"
-  echo ${bold_red}ERROR:${normal} $msg
+  echo -e ${bold_red}ERROR:${normal} $msg
 }
 
 function print_warning() {
   local msg=$1
   local bold_yellow="\033[1;33m"
   local normal="\033[0m"
-  echo ${bold_yellow}WARNING:${normal} $msg
+  echo -e ${bold_yellow}WARNING:${normal} $msg
 }
 
 function print_hint() {
   local msg=$1
   local bold_cyan_bkg="\033[1;46m"
   local normal="\033[0m"
-  echo ${bold_cyan_bkg}HINT${normal}: $msg
+  echo -e ${bold_cyan_bkg}HINT${normal}: $msg
 }
 
 function print_title() {
   local msg=$1
   local bold_green="\033[1;32m"
   local normal="\033[0m"
-  echo "\n${bold_green}$msg\n----------------------------------------\n${normal}"
+  echo -e "\n${bold_green}$msg\n----------------------------------------\n${normal}"
 }
 
 function command_exists() {
